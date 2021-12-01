148be73f89f3bfb3568403a37bfcea8e3f8362ae
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2021-12-01 01:30:07 -0600

    Vistas nuevas

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -7,19 +7,19 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Juan", "Andrea", "Marcos", "Abril", "Ariadna", "Miguel"],
     datasets: [{
-      label: "Revenue",
+      label: "% de avance",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [50, 85, 10, 24, 38, 2],
     }],
   },
   options: {
     scales: {
       xAxes: [{
         time: {
-          unit: 'month'
+          unit: 'percentage'
         },
         gridLines: {
           display: false
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 100,
           maxTicksLimit: 5
         },
         gridLines: {
@@ -40,7 +40,7 @@ var myLineChart = new Chart(ctx, {
       }],
     },
     legend: {
-      display: false
+      display: true
     }
   }
 });
